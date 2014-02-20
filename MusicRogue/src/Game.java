af89aea161df7267a9ca13ac02a071fe2fee47c0
kylejrp <kylejrp@gmail.com>
2014-02-20 15:22:42 -0800

    First Commit
    Basic structure of the Game class.

---
+++ MusicRogue/src/Game.java
@@ -0,0 +1,30 @@
+public class Game implements Runnable {
+	public Game() {
+
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			getInput();
+			update();
+			render();
+		}
+	}
+
+	private void render() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void update() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void getInput() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
