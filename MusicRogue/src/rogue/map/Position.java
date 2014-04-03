9bf830b4efca984e45d9791de128f7b124d80245
kylejrp <kylejrp@gmail.com>
2014-04-03 02:50:57 -0700

    Implemented a equals method for Position How did we make it this far I'm so sorry Conan

--- MusicRogue/src/rogue/map/Position.java
+++ MusicRogue/src/rogue/map/Position.java
@@ -44,4 +44,14 @@ public class Position {
 		return "(" + x + ", "+ y + ")";
 	}
 	
+	@Override
+	public boolean equals(Object o){
+		if(o instanceof Position){
+			Position pos = (Position) o;
+			return (pos.getX() == this.getX()) && (pos.getY() == this.getY());
+		} else {
+			return false;
+		}
+	}
+	
 }
